a32b0bde6a16245dd1cc6ca98b335f4f069fc50e
MonsieurBoubou <bouvierleo26@gmail.com>
2025-06-06 23:07:04 +0200

    ça avance sur les tests du voisinage

--- src/scheduling/optim/neighborhoods.py
+++ src/scheduling/optim/neighborhoods.py
@@ -4,6 +4,7 @@ They must derive from the Neighborhood class.
 
 @author: Vassilissa Lehoux
 '''
+import copy
 from typing import Dict
 
 from src.scheduling.instance.instance import Instance
@@ -37,53 +38,152 @@ class Neighborhood(object):
         raise "Not implemented error"
 
 
-class MyNeighborhood1(Neighborhood):
+class MachineSwitchNeighborhood(Neighborhood):
     '''
-    Give the Neighborhood a meaningful name and add here a short description.
-    '''
-
-    def __init__(self, instance: Instance, params: Dict=dict()):
-        '''
-        Constructor
-        '''
-        raise "Not implemented error"
-
-    def best_neighbor(self, sol: Solution) -> Solution:
-        '''
-        Returns the best solution in the neighborhood of the solution.
-        Can be the solution itself.
-        '''
-        raise "Not implemented error"
-
-    def first_better_neighbor(self, sol: Solution) -> Solution:
-        '''
-        Returns the first solution in the neighborhood of the solution
-        that improves other it and the solution itself if none is better.
-        '''
-        raise "Not implemented error"
-
-
-class MyNeighborhood2(Neighborhood):
-    '''
-    Give the Neighborhood a meaningful name and add here a short description.
+    Premier voisinage : change la machine sur laquelle s'exécute une opération.
+    Taille du voisinage : O(O * (M-1)) où O est le nombre d'opérations et M le nombre de machines.
     '''
 
     def __init__(self, instance: Instance, params: Dict=dict()):
         '''
         Constructor
         '''
-        raise "Not implemented error"
+        super().__init__(instance,params)
 
     def best_neighbor(self, sol: Solution) -> Solution:
         '''
         Returns the best solution in the neighborhood of the solution.
         Can be the solution itself.
         '''
-        raise "Not implemented error"
+        best_solution = sol
+        best_value = sol.evaluate
+
+        for operation in sol.all_operations:
+            current_machine_id = operation.assigned_to
+            for machine in self._instance.machines:
+                if machine.machine_id != current_machine_id:
+                    # Créer une nouvelle solution
+                    neighbor = copy.deepcopy(sol)
+                    # Trouver l'opération correspondante dans la nouvelle solution
+                    op = next(op for op in neighbor.all_operations if op.operation_id == operation.operation_id)
+                    # Réassigner l'opération à la nouvelle machine
+                    op.reset()
+                    neighbor.schedule(op, machine)
+
+                    # Évaluer le voisin
+                    neighbor_value = neighbor.evaluate
+                    if neighbor_value < best_value:
+                        best_solution = neighbor
+                        best_value = neighbor_value
+
+        return best_solution
 
     def first_better_neighbor(self, sol: Solution) -> Solution:
         '''
         Returns the first solution in the neighborhood of the solution
         that improves other it and the solution itself if none is better.
         '''
-        raise "Not implemented error"
+        current_value = sol.evaluate
+
+        for operation in sol.all_operations:
+            current_machine_id = operation.assigned_to
+            for machine in self._instance.machines:
+                if machine.machine_id != current_machine_id:
+                    # Créer une nouvelle solution
+                    neighbor = copy.deepcopy(sol)
+                    # Trouver l'opération correspondante dans la nouvelle solution
+                    op = next(op for op in neighbor.all_operations if op.operation_id == operation.operation_id)
+                    # Réassigner l'opération à la nouvelle machine
+                    op.reset()
+                    neighbor.schedule(op, machine)
+
+                    # Si le voisin est meilleur, le retourner immédiatement
+                    if neighbor.evaluate < current_value:
+                        return neighbor
+
+        return sol
+
+
+
+class OperationOrderNeighborhood(Neighborhood):
+    '''
+    Deuxième voisinage : Permutations d'ordre sur une machine.
+    Taille du voisinage : O(N^2) dans le pire cas, où N est le nombre total d'opérations.
+    '''
+
+
+    def __init__(self, instance: Instance, params: Dict=dict()):
+        '''
+        Constructor
+        '''
+        super().__init__(instance, params)
+
+    def best_neighbor(self, sol: Solution) -> Solution:
+        '''
+        Returns the best solution in the neighborhood of the solution.
+        Can be the solution itself.
+        '''
+        best_solution = sol
+        best_value = sol.evaluate
+
+        for machine in self._instance.machines:
+            # Récupérer toutes les opérations sur cette machine
+            machine_ops = [op for op in sol.all_operations if op.assigned_to == machine.machine_id]
+
+            # Pour chaque paire d'opérations sur la machine
+            for i in range(len(machine_ops)):
+                for j in range(i + 1, len(machine_ops)):
+                    # Créer une nouvelle solution
+                    neighbor = _create_neighbor_solution(sol, machine_ops[i], machine_ops[j], machine)
+
+                    # Évaluer le voisin
+                    neighbor_value = neighbor.evaluate
+                    if neighbor_value < best_value:
+                        best_solution = neighbor
+                        best_value = neighbor_value
+
+        return best_solution
+
+    def first_better_neighbor(self, sol: Solution) -> Solution:
+        '''
+        Returns the first solution in the neighborhood of the solution
+        that improves other it and the solution itself if none is better.
+        '''
+        current_value = sol.evaluate
+
+        for machine in self._instance.machines:
+            # Récupérer toutes les opérations sur cette machine
+            machine_ops = [op for op in sol.all_operations if op.assigned_to == machine.machine_id]
+
+            # Pour chaque paire d'opérations sur la machine
+            for i in range(len(machine_ops)):
+                for j in range(i + 1, len(machine_ops)):
+                    # Créer une nouvelle solution
+                    neighbor = _create_neighbor_solution(sol, machine_ops[i], machine_ops[j], machine)
+
+                    # Si le voisin est meilleur, le retourner immédiatement
+                    if neighbor.evaluate < current_value:
+                        return neighbor
+
+        return sol
+
+
+def _create_neighbor_solution(sol: Solution, op1, op2, machine):
+    '''
+    Helper function to create a neighbor solution by swapping two operations.
+    '''
+    neighbor = copy.deepcopy(sol)
+
+    # Trouver les opérations correspondantes dans la nouvelle solution
+    op1_neighbor = next(op for op in neighbor.all_operations if op.operation_id == op1.operation_id)
+    op2_neighbor = next(op for op in neighbor.all_operations if op.operation_id == op2.operation_id)
+
+    # Réinitialiser les assignations
+    op1_neighbor.reset()
+    op2_neighbor.reset()
+
+    # Réassigner les opérations dans l'ordre inverse
+    neighbor.schedule(op2_neighbor, machine)
+    neighbor.schedule(op1_neighbor, machine)
+
+    return neighbor
---
+++ src/scheduling/tests/test_neighborhoods.py
@@ -0,0 +1,79 @@
+import unittest
+import os
+from src.scheduling.instance.instance import Instance
+from src.scheduling.solution import Solution
+from src.scheduling.optim.neighborhoods import MachineSwitchNeighborhood, OperationOrderNeighborhood
+from src.scheduling.tests.test_utils import TEST_FOLDER_DATA
+
+
+
+def create_initial_solution():
+    """Helper pour créer une nouvelle solution initiale"""
+    inst = Instance.from_file(TEST_FOLDER_DATA + os.path.sep + "jsp_test_neighborhoods")
+    solution = Solution(inst)
+
+    # Planifier plusieurs opérations sur différentes machines
+    operations_to_schedule = [
+        (0, 0), (0, 1), (0, 2),  # Job 0
+        (1, 3), (1, 4), (1, 5),  # Job 1
+        (2, 6), (2, 7), (2, 8)   # Job 2
+    ]
+
+    for job_id, op_id in operations_to_schedule:
+        operation = next(op for op in inst.operations
+                       if op.job_id == job_id and op.operation_id == op_id)
+        machine = inst.machines[0]
+        solution.schedule(operation, machine)
+
+    return inst, solution
+
+class TestNeighborhoods(unittest.TestCase):
+
+    def test_machine_switch_best_neighbor(self):
+        """Tester la recherche du meilleur voisin pour MachineSwitchNeighborhood"""
+        inst, solution = create_initial_solution()
+        neighborhood = MachineSwitchNeighborhood(inst)
+        initial_value = solution.evaluate
+        
+        best_sol = neighborhood.best_neighbor(solution)
+        
+        self.assertLessEqual(best_sol.evaluate, initial_value)
+        self.assertTrue(best_sol.is_feasible)
+
+        
+    def test_operation_order_best_neighbor(self):
+        """Tester la recherche du meilleur voisin pour OperationOrderNeighborhood"""
+        inst, solution = create_initial_solution()
+        neighborhood = OperationOrderNeighborhood(inst)
+        initial_value = solution.evaluate
+        
+        best_sol = neighborhood.best_neighbor(solution)
+        
+        self.assertLessEqual(best_sol.evaluate, initial_value)
+        self.assertTrue(best_sol.is_feasible)
+        
+    def test_machine_switch_first_better(self):
+        """Tester la recherche du premier voisin améliorant pour MachineSwitchNeighborhood"""
+        inst, solution = create_initial_solution()
+        neighborhood = MachineSwitchNeighborhood(inst)
+        initial_value = solution.evaluate
+        
+        better_sol = neighborhood.first_better_neighbor(solution)
+        
+        self.assertLessEqual(better_sol.evaluate, initial_value)
+        self.assertTrue(better_sol.is_feasible)
+        
+    def test_operation_order_first_better(self):
+        """Tester la recherche du premier voisin améliorant pour OperationOrderNeighborhood"""
+        # Planifier une deuxième opération sur la même machine
+        inst, solution = create_initial_solution()
+        neighborhood = OperationOrderNeighborhood(inst)
+        initial_value = solution.evaluate
+        
+        better_sol = neighborhood.first_better_neighbor(solution)
+        
+        self.assertLessEqual(better_sol.evaluate, initial_value)
+        self.assertTrue(better_sol.is_feasible)
+
+if __name__ == '__main__':
+    unittest.main()
